3f9239dd0613c449520f60c4b2c41922631bb925
abhixvix <abhimanyutambde1111@gmail.com>
2025-01-11 16:43:31 +0530

    Add files via upload

---
+++ practise.js
@@ -0,0 +1,15 @@
+// //  git add practise.js
+// //  git commit -m added practise.js
+
+
+// // let str = "Hello";
+// // console.log(str.length);
+
+// // let str = "JavaScript";
+// // console.log(str.slice(0, 4)); 
+
+// let s = "JavaScript";
+// b=s.slice(2,4);
+// console.log(b)
+
+
